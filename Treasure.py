4afba5be6cbfd48b050399975e486a6c486538c9
dbastian <bd@pos.ag>
2025-10-18 03:47:47 +0200

    fixed maze size

--- Treasure.py
+++ Treasure.py
@@ -6,7 +6,7 @@ clear()
 
 while True:
 	plant(Entities.Bush)
-	use_item(Items.Weird_Substance, ws)
+	use_item(Items.Weird_Substance, ws * 2)
 	while get_entity_type() != Entities.Treasure:
 		dir = (dir + 2 * move(dirs[dir]) - 1) % 4
 	harvest()
