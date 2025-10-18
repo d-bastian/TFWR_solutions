b6771f3cf0b94f059d065b51d8dd213049ce7cec
dbastian <bd@pos.ag>
2025-10-18 03:32:07 +0200

    Added var for min_sunflower

--- Sunflower.py
+++ Sunflower.py
@@ -8,7 +8,7 @@ while True:
 	for x in range(ws):
 		for y in range(ws):
 			plant(Entities.Sunflower)
-			if get_entity_type() == Entities.Sunflower and measure() > 10:
+			if get_entity_type() == Entities.Sunflower and measure() > min_flower:
 				list.append((measure(),get_pos()))
 			move(North)
 			check_water()
